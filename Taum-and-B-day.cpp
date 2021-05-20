350a109f8ee3b9567a81c95697753532120a12d2
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2021-05-21 03:54:52 +0900

    Update Taum-and-B-day.cpp

--- Taum-and-B-day.cpp
+++ Taum-and-B-day.cpp
@@ -1,1 +1,13 @@
+long taumBday(int b, int w, int bc, int wc, int z)
+{
+    return b * long(std::min(bc, wc+z))  +  w * long(std::min(wc, bc+z));
+}
 
+
+long taumBday1(int b, int w, int bc, int wc, int z)
+{
+    long bc1 = bc < wc+z? bc : wc+z;
+    long wc1 = wc < bc+z? wc : bc+z;
+
+    return (b*bc1) + (w*wc1);
+}
