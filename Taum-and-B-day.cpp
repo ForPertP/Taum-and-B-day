b154cb4bd2a666bd8c6c7a90a22affcc763a132b
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2023-03-22 07:12:37 +0900

    Update Taum-and-B-day.cpp

--- Taum-and-B-day.cpp
+++ Taum-and-B-day.cpp
@@ -1,3 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'taumBday' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER b
+ *  2. INTEGER w
+ *  3. INTEGER bc
+ *  4. INTEGER wc
+ *  5. INTEGER z
+ */
+
 long taumBday(int b, int w, int bc, int wc, int z)
 {
     return b * long(std::min(bc, wc+z))  +  w * long(std::min(wc, bc+z));
