bdfc3c168de533c198a06b6b12e252be3e256385
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2023-03-23 00:45:55 +0900

    Update Taum-and-B-day.cpp

--- Taum-and-B-day.cpp
+++ Taum-and-B-day.cpp
@@ -31,3 +31,43 @@ long taumBday1(int b, int w, int bc, int wc, int z)
 
     return (b*bc1) + (w*wc1);
 }
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string t_temp;
+    getline(cin, t_temp);
+
+    int t = stoi(ltrim(rtrim(t_temp)));
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        string first_multiple_input_temp;
+        getline(cin, first_multiple_input_temp);
+
+        vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+        int b = stoi(first_multiple_input[0]);
+
+        int w = stoi(first_multiple_input[1]);
+
+        string second_multiple_input_temp;
+        getline(cin, second_multiple_input_temp);
+
+        vector<string> second_multiple_input = split(rtrim(second_multiple_input_temp));
+
+        int bc = stoi(second_multiple_input[0]);
+
+        int wc = stoi(second_multiple_input[1]);
+
+        int z = stoi(second_multiple_input[2]);
+
+        long result = taumBday(b, w, bc, wc, z);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
